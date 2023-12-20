b7a1c24b25bd1563bdc3cc1633e98a025abee20b
jo-hannes <jo-hannes@dev-urandom.de>
2023-12-20 21:06:20 +0100

    Style: Derive from QAbstractTableModel

--- src/model/style.cpp
+++ src/model/style.cpp
@@ -15,6 +15,50 @@ Style::Style(QString name) {
   }
 }
 
+Style::Style(const QJsonObject& json) {
+  name = json["Name"].toString("");
+  // start at index 1 to skip volume
+  for (int i = 1; i < static_cast<int>(AM::WaterValue::Size); i++) {
+    // get sub object
+    QJsonValue limit = json[AM::waterStrings[i][AM::JsonKey]];
+    limited[i] = limit.isObject();
+    for (int j = 0; j < static_cast<int>(Limit::Size); j++) {
+      limits[i][j] = limit[jsonKeys[j]].toDouble(0);
+    }
+  }
+}
+
+QJsonObject Style::toJson() const {
+  QJsonObject json;
+  json["Name"] = name;
+  // start at index 1 to skip volume
+  for (int i = 1; i < static_cast<int>(AM::WaterValue::Size); i++) {
+    // skip values not limited
+    if (!limited[i]) {
+      continue;
+    }
+    // create limit object
+    QJsonObject limit;
+    for (int j = 0; j < static_cast<int>(Limit::Size); j++) {
+      limit[jsonKeys[j]] = limits[i][j];
+    }
+    // add object to main json
+    json[AM::waterStrings[i][AM::JsonKey]] = limit;
+  }
+  return json;
+}
+
+Style* Style::copy() const {
+  Style* copy = new Style(this->getName());
+  for (int i = 0; i < static_cast<int>(AM::WaterValue::Size); i++) {
+    copy->limited[i] = this->limited[i];
+    for (int j = 0; j < static_cast<int>(Limit::Size); j++) {
+      copy->limits[i][j] = this->limits[i][j];
+    }
+  }
+  return copy;
+}
+
 QString Style::getName() const {
   return name;
 }
@@ -51,38 +95,100 @@ void Style::limit(AM::WaterValue what, bool limit)
   }
 }
 
-Style Style::fromJson(const QJsonObject& json) {
-  Style ret;
-  ret.name = json["Name"].toString("");
-
-  // start at index 1 to skip volume
-  for (int i = 1; i < static_cast<int>(AM::WaterValue::Size); i++) {
-    // get sub object
-    QJsonValue limit = json[AM::waterStrings[i][AM::JsonKey]];
-    ret.limited[i] = limit.isObject();
-    for (int j = 0; j < static_cast<int>(Limit::Size); j++) {
-      ret.limits[i][j] = limit[jsonKeys[j]].toDouble(0);
-    }
-  }
-  return ret;
+int Style::rowCount(const QModelIndex& parent) const {
+  Q_UNUSED(parent);
+  // limits without volume
+  return static_cast<int>(AM::WaterValue::Size) - 1;
 }
 
-QJsonObject Style::toJson() const {
-  QJsonObject json;
-  json["Name"] = name;
-  // start at index 1 to skip volume
-  for (int i = 1; i < static_cast<int>(AM::WaterValue::Size); i++) {
-    // skip values not limited
-    if (!limited[i]) {
-      continue;
-    }
-    // create limit object
-    QJsonObject limit;
-    for (int j = 0; j < static_cast<int>(Limit::Size); j++) {
-      limit[jsonKeys[j]] = limits[i][j];
-    }
-    // add object to main json
-    json[AM::waterStrings[i][AM::JsonKey]] = limit;
+int Style::columnCount(const QModelIndex& parent) const {
+  Q_UNUSED(parent);
+  // Enable disable + Limits
+  return 1 + static_cast<int>(Limit::Size);
+}
+
+QVariant Style::data(const QModelIndex& index, int role) const {
+  if (!index.isValid()) {
+    return QVariant();
   }
-  return json;
+  if (role != Qt::DisplayRole) {
+    return QVariant();
+  }
+  qsizetype row = index.row() + 1;  // Skip volume
+  if (row < 0 || row >= static_cast<int>(AM::WaterValue::Size)) {
+    return QVariant();
+  }
+  qsizetype col = index.column();
+  // enable disable
+  if (col == 0) {
+    return limited[row];
+  }
+  // values
+  if (col > 0 && col < static_cast<int>(Limit::Size) + 1) {
+    return limits[row][col - 1];
+  }
+  return QVariant();
+}
+
+QVariant Style::headerData(int section, Qt::Orientation orientation, int role) const {
+  if (role != Qt::DisplayRole) {
+    return QVariant();
+  }
+  if (orientation == Qt::Horizontal) {
+    // enable disable
+    switch (section) {
+      case 0:
+        return tr("Limitieren");
+      case 1:
+        return tr("Untergrenze");
+      case 2:
+        return tr("Untere Empfehlung");
+      case 3:
+        return tr("Obere Empfehlung");
+      case 4:
+        return tr("Obergrenze");
+      default:
+        return QVariant();
+    }
+  } else {
+    int idx = section + 1;  // skip volume
+    if (idx > 0 && idx < static_cast<int>(AM::WaterValue::Size)) {
+      return AM::waterStrings[idx][AM::JsonKey];
+    }
+  }
+  return QVariant();
+}
+
+bool Style::setData(const QModelIndex& index, const QVariant& value, int role) {
+  if (!index.isValid()) {
+    return false;
+  }
+  if (role != Qt::EditRole) {
+    return false;
+  }
+  qsizetype row = index.row() + 1;  // Skip volume
+  if (row < 0 || row >= static_cast<int>(AM::WaterValue::Size)) {
+    return false;
+  }
+  qsizetype col = index.column();
+  // enable disable
+  if (col == 0) {
+    limited[row] = value.toBool();
+    return true;
+  }
+  // values
+  if (col > 0 && col < static_cast<int>(Limit::Size) + 1) {
+    limits[row][col - 1] = value.toFloat();
+    return true;
+  }
+  // mark as unsaved!!
+  // setUnsaved(true);
+  return false;
+}
+
+Qt::ItemFlags Style::flags(const QModelIndex& index) const {
+  if (!index.isValid()) {
+    return Qt::NoItemFlags;
+  }
+  return Qt::ItemIsEditable | QAbstractItemModel::flags(index);
 }
--- src/model/style.h
+++ src/model/style.h
@@ -6,14 +6,16 @@
 
 #include "../global.h"
 
+#include <QAbstractTableModel>
 #include <QJsonObject>
 #include <QString>
 
 /**
  * @brief Class for a single beer style
  */
-class Style
-{
+class Style : public QAbstractTableModel {
+  Q_OBJECT
+
  public:
   /**
    * @brief Type of limits
@@ -21,6 +23,12 @@ class Style
   enum class Limit { LowerLimit, LowerRecomendation, UpperRecomendation, UpperLimit, Size };
 
   Style(QString name = "");
+
+  // JSON conversion
+  Style(const QJsonObject& json); /**< @brief Construct Style from JSON */
+  QJsonObject toJson() const;     /**< @brief convert Style to JSON */
+
+  Style* copy() const; /**< @brief create a copy*/
 
   // getter und setter
   QString getName() const;
@@ -30,11 +38,15 @@ class Style
   bool isLimited(AM::WaterValue what);                     /**< @brief is value limited */
   void limit(AM::WaterValue what, bool limit);             /**< @brief Limit value */
 
-  // JSON conversion
-  static Style fromJson(const QJsonObject& json); /**< @brief convert JSON to Style */
-  QJsonObject toJson() const;                     /**< @brief convert this Style to JSON */
+  // for QAbstractTableModel, see QT documentation for details
+  int rowCount(const QModelIndex& parent = QModelIndex()) const;
+  int columnCount(const QModelIndex& parent = QModelIndex()) const;
+  QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const;
+  QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const;
+  bool setData(const QModelIndex& index, const QVariant& value, int role = Qt::EditRole);
+  Qt::ItemFlags flags(const QModelIndex& index) const;
 
- protected:
+ private:
   QString name;
   bool limited[static_cast<int>(AM::WaterValue::Size)];
   float limits[static_cast<int>(AM::WaterValue::Size)][static_cast<int>(Limit::Size)];
