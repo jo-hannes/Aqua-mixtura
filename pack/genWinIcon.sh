c1a37ef3f586897354dabc4995eee84b70573ddf
Johannes Eigner <jo-hannes@dev-urandom.de>
2024-11-07 23:57:22 +0100

    Add windows ci build (#2)

---
+++ pack/genWinIcon.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# SPDX-License-Identifier: GPL-2.0-only
+# Copyright (c) 2024 jo-hannes <jo-hannes@dev-urandom.de>
+
+magick ../src/icons/logo_512x512.png -define icon:auto-resize=256,128,48,32,16 icon.ico
