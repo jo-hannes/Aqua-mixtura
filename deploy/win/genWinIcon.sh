5332d17595319825508f76ef25fe68c9d599d188
jo-hannes <jo-hannes@dev-urandom.de>
2025-03-29 00:07:59 +0100

    Restructure deploy directory

--- deploy/win/genWinIcon.sh
+++ deploy/win/genWinIcon.sh
@@ -2,4 +2,4 @@
 # SPDX-License-Identifier: GPL-2.0-only
 # Copyright (c) 2024 jo-hannes <jo-hannes@dev-urandom.de>
 
-magick ../src/icons/logo_512x512.png -define icon:auto-resize=256,128,48,32,16 icon.ico
+magick ../../src/icons/logo_512x512.png -define icon:auto-resize=256,128,48,32,16 icon.ico
